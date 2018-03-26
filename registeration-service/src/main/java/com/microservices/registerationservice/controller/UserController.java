d6a2b92b2b5e944fe87b451409cfa16e410e58b9
Anish <anishkap7010@gmail.com>
2018-03-26 23:28:37 +0530

    Crud operations on registeration service working

--- registeration-service/src/main/java/com/microservices/registerationservice/controller/UserController.java
+++ registeration-service/src/main/java/com/microservices/registerationservice/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.microservices.registerationservice.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +21,7 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@PostMapping("/register-user")
+	@PostMapping("/users")
 	public void addUser(@RequestBody User user) {
 		userRepository.save(user);
 	}
@@ -26,4 +30,21 @@ public class UserController {
 	public List<User> showAllUsers() {
 		return userRepository.findAll();
 	}
+	
+	@GetMapping("/users/{id}")
+	public Optional<User> showUsers(@PathVariable int id) {
+		 Optional<User> user = userRepository.findById(id);
+		 return user;
+	}
+	
+	@PutMapping("/users/{id}")
+	public void updateUser(@RequestBody User user) {
+		userRepository.save(user);
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		userRepository.deleteById(id);
+	}
+	
 }
